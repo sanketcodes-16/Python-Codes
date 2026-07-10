2e39b69f5bb7287c13ef75531722a51976f2bb39
sanketcodes-16 <msanket354@gmail.com>
2026-07-10 22:58:23 +0530

    Self keyword and Constructor.

--- oop_revision.py
+++ oop_revision.py
@@ -96,5 +96,137 @@ print("Location :",store.location)
 print("Products :",store.products)
 =============================================================================================================================
 
+# What is self ? 
+Ans : Self is a reference keyword which allows u to access 
+members of a class.
+means whatever is there inide a class that we can access
+
+self is an object inside a class who gives an access to all the members of the class.
+
+------------------------------------------------------------------------------------------------------------
+class info:
+    name = 'python'  # class variable
+    age = 35
+    
+    def display(self):
+        print ('Name is :',self.name)
+        print("Age is :",self.age)
+        
+obj = info()
+print(obj.name,obj.age)
+obj.display()
+=====================================================================================================================
+
+class info:
+    name = 'python'  # class variable
+    age = 35
+    
+    def display(self):
+        print ('Name is :',self.name)
+        print("Age is :",self.age)
+        
+    def call(self):
+        self.display()
+        
+obj = info()
+print(obj.name,obj.age)
+# call ---> display
+obj.call()
+=============================================================================================
+
+class Bank:
+    def credit(self):
+        print('Credit Method')
+        
+    def debit(self):
+        print("Debit Method")
+        
+    def transaction(self):
+        self.credit()
+        self.debit()
+        
+obj = Bank()
+# transaction ---> credit,debit
+obj.transaction()
+==========================================================================
+
+Constructor : Class calling is a constructor calling.
+----------------------------------------------------------------
+
+class Bank:
+    def __init__(self):
+        print("Constructor of a Class Bank")
+
+obj = Bank()
+# obj is an object of the Bank
+# Bank() is a constructor of class Bank
+Bank()
+Bank()
+====================================================================================
+
+# Purpose of constructor is to initialize memory or allocate memory
+class Bank:
+    def __init__(self):     # constructor called automatically
+        print("Constructor of a Class Bank")
+        
+    def sample(self):     # We need to create an object of this class to call this method
+        print("Sample method")
+
+obj = Bank()
+# obj is an object of the Bank
+# Bank() is a constructor of class Bank
+Bank()
+Bank()
+================================================================================================================
+
+## How init help in programming :
+
+class Bank:
+    def __init__(self,nm,ag,sal):   # dunder method  ---> __method__
+        print("Constructor of a Class Bank")
+        self.name = nm
+        self.age = ag
+        self.salary = sal
+        
+    def display(self):
+        print("Name is :",self.name)
+        print("Age is :",self.age)
+        print("Salary is :",self.salary)
+        
+    def record(self):
+        print("Person name on Record :",self.name)
+        
+obj = Bank('Sanket',30,50000)
+obj.display()
+obj.record()
+===========================================================================================================
+
+class Human:
+    def __init__(self,head,hands,eyes):
+        self.head = head
+        self.hands = hands
+        self.eyes = eyes
+        
+    def display(self):
+        print("Head :",self.head)
+        print("Hands :",self.hands)
+        print("Eyes :",self.eyes)
+        
+person = Human(1,2,2)
+person.display()
+print("-"*50)
+
+alians = Human(5,10,20)
+alians.hands = 5
+alians.display()
+print("-"*50)
+
+ravan = Human(1,2,2)
+ravan.head = 10
+ravan.hands = 20
+ravan.display()
+print("-"*50)
+====================================================================================================
+
 
 """
